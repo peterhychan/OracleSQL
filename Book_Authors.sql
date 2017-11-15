c406da31f18ba1a414ace80be268dae7d195049e
Peter Ho Yeung Chan <peterchan01272002@gmail.com>
2017-11-15 14:22:45 -0800

    Updated DB
    Updated DB

---
+++ Book_Authors.sql
@@ -0,0 +1,11 @@
+insert into bk_books values (9010, 'Intruction to XML', 9745, 2017, '99901', 100, 200.00);
+insert into bk_book_authors values (9010, 'B1144', 1);
+
+insert into bk_books values (9011, 'Oracle SQL Procedure', 9745, 2017, '99902', 150, 110.00);
+insert into bk_book_authors values (9011, 'S2144', 1);
+
+insert into bk_books values (9012, 'HTML + Javascript', 9745, 2017, '99903', 220, 50.00);
+insert into bk_book_authors values (9012, 'W4512', 1);
+
+insert into bk_books values (9013, 'HTML5 for Dummies', 9745, 2017, '99904', 265, 65.00);
+insert into bk_book_authors values (9013, 'B1244', 1);
