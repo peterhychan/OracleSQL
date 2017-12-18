6184bd98b7000caf75a8d8b9856940b38cb74e83
Peter Ho Yeung Chan <peterchan01272002@gmail.com>
2017-12-18 00:12:18 -0800

    A13 Adjustment
    A13 Adjustment

--- A13_chan/A13_chan.sql
+++ A13_chan/A13_chan.sql
@@ -70,59 +70,38 @@ from XML_Table
 ;
 
 /*  TASK 04 */ 
-with XML_Table as 
-	(select extractvalue(A14.OBJECT_VALUE, '/book/bk_id') as BookID 
-	, extractvalue(A14.OBJECT_VALUE, '/book/bk_title') as Title 
-	, extractvalue(A14.OBJECT_VALUE, '/book/bk_price') as Price 		, extract(A14.OBJECT_VALUE, '/book/bk_authors') as AuthorList
-	from xml_A13_books A14
-	where A14.OBJECT_VALUE.extract('//bk_authors').getStringVal() like '%>Martin<%')
-select	BookID as "BookID"
-, Title as "Title"
-, AuthorList as "AuthorList"
-from XML_Table
+select extractvalue(object_value, '//bk_id') "BookID"
+, extractvalue(object_value, '//bk_title') "Title"
+, extract(object_value, '//author') "AuthorList"
+from xml_A13_books
+where existsNode(object_value, '//bk_authors[author="Martin"]')=1
 ;
 
 /*  TASK 05 */
-with XML_Table as 
-	(select extractvalue(A14.OBJECT_VALUE, '/book/bk_id') as BookID 
-	, extractvalue(A14.OBJECT_VALUE, '/book/bk_title') as Title 
-	, extractvalue(A14.OBJECT_VALUE, '/book/bk_price') as Price
-	, extract(A14.OBJECT_VALUE, '/book/bk_authors') as AuthorList
-	from xml_A13_books A14
-	where A14.OBJECT_VALUE.extract('//author[1]/text()').getStringVal() = 'Martin')
-select BookID as "BookID"
-, Title as "Title"
-, AuthorList as "AuthorList"
-from XML_Table
+select extractvalue(object_value, '//bk_id') "BookID"
+, extractvalue(object_value, '//bk_title') "Title"
+, extract(object_value, '//author') "AuthorList"
+from xml_A13_books
+where extractvalue(object_value, '//author[1]') = 'Martin'
 ;
 
 /*  TASK 06 */
-with XML_Table as 
-	(select extractvalue(A14.OBJECT_VALUE, '/book/bk_id') as BookID 
-	, extractvalue(A14.OBJECT_VALUE, '/book/bk_title') as Title 
-	, extractvalue(A14.OBJECT_VALUE, '/book/bk_price') as Price 
-	, extract(A14.OBJECT_VALUE, '/book/bk_authors') as AuthorList
-	from xml_A13_books A14
-	where A14.OBJECT_VALUE.extract('//bk_authors').getStringVal() not like '%>Martin<%')
-select BookID as "BookID"
-, Title as "Title"
-, AuthorList as "AuthorList"
-from XML_Table
+select
+  extractvalue(object_value, '//bk_id') "BookID"
+, extractvalue(object_value, '//bk_title') "Title"
+, extract(object_value, '//author') "AuthorList"
+from xml_A13_books
+where existsNode(object_value, '//bk_authors[author="Martin"]')=0
 ;
 
+
 /*  TASK 07 */
-with XML_Table as 
-	(select extractvalue(A14.OBJECT_VALUE, '/book/bk_id') as BookID 
-	, extractvalue(A14.OBJECT_VALUE, '/book/bk_title') as Title 
-	, extract(A14.OBJECT_VALUE, '/book/bk_authors') as AuthorList
-	, (select count(*)
-	   from table(xmlsequence(extract(A14.OBJECT_VALUE, '/book/bk_authors/author')))) 
-	   as NumberOfAuthors
-	from xml_A13_books A14)
-select BookID as "BookID"
-, Title as "Title"
-from XML_Table
-where NumberOfAuthors between 4 and 10
+select
+  extractvalue(object_value, '//bk_id') BookID
+, extractvalue(object_value, '//bk_title') Title
+from xml_A13_books
+where existsNode(object_value, '//author[4]')=1    --- at least 4 authors
+and   existsNode(object_value, '//author[11]')=0   --- no more than 10 authors
 ;
 
 /*  TASK 08 */
@@ -162,23 +141,40 @@ from TMP)
 ;
 
 /*  TASK 10 */
-with XML_Table as 
-	(select extractvalue(OBJECT_VALUE, '/book/bk_subject') as Subject
-	, cast(extractvalue(OBJECT_VALUE, '/book/bk_price') as number) as Price 
-	, extractvalue(A14.OBJECT_VALUE, '/book/bk_id') as BookID 
-	from xml_A13_books A14)
-, TMP as 
-	(select Subject 
-	, nvl(avg(Price), 0.00) as AvgPrice
-	from XML_Table
-	group by Subject)
-select (
-  select to_char(avg(AvgPrice), '999,999,999.99')  
-  from TMP 
-  where subject = 'SQL') as "AvgPrice_SQL"
-, (select to_char(avg(AvgPrice), '999,999,999.99') 
-from TMP 
-where Subject != 'SQL') as "AvgPrice_Other"
-from dual
+select
+  round(avg( 
+    case when subject='SQL' then price end ), 2) "AvgPrice_SQL"
+, round(avg( 
+    case when subject!='SQL' then price end ), 2) "AvgPrice_Other"
+from(
+  select
+    extractvalue(object_value, '//bk_subject') Subject
+  , extractvalue(object_value, '//bk_price') Price
+  from xml_A13_books
+);
+
+
+/*
+Another method for task 10:
+
+With XML_Table as
+(select extractvalue(OBJECT_VALUE, '/book/bk_subject') as Subject
+, cast(extractvalue(OBJECT_VALUE, '/book/bk_price') as number) as Price
+, extractvalue(A14.OBJECT_VALUE, '/book/bk_id') as BookID
+from xml_A13_books A14)
+, TMP1 as
+(select nvl(avg(Price), 0.00) as AvgPrice
+from XML_Table
+where Subject = 'SQL')
+, TMP2 as
+(select nvl(avg(Price), 0.00) as AvgPrice
+from XML_Table
+where Subject != 'SQL')
+select to_char(TMP1.AvgPrice, '999,999,999.99') as "AvgPrice_SQL"
+, to_char(TMP2.AvgPrice, '999,999,999.99') as "AvgPrice_Other"
+from TMP1, TMP2
 ;
+ */
+
+
 
